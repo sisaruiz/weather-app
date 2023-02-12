08e34997f8bbeb1d83c88d6a296b67b8df6ea7da
Sisa Ruiz <sisaruizm@gmx.com>
2023-02-12 22:59:18 +0100

    fetch API data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-<div id="app">
+<div id="app" :class="(typeof weather.main != 'undefined' && weather.main.temp > 16 ? 
+'warm' : '')">
   <main>
     <div class="search-box">
-      <input type="text" class="search-bar" placeholder="Search.."/>
+      <input
+        type="text" 
+        class="search-bar" 
+        placeholder="Search.."
+        v-model="query"
+        @keypress="fetchWeather"
+      />
     </div>
-    <div class="weather-wrap">
+    <div class="weather-wrap" v-if="(typeof weather.main != 'undefined')">
       <div class="location-box">
-        <div class="location">Peguche, EC</div>
-        <div class="date">Sunday 12 February 2023</div>
+        <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+        <div class="date">{{ dateBuilder() }}</div>
       </div>
       <div class="weather-box">
-        <div class="temp">19° c</div>
-        <div class="weather">Rain</div>
+        <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+        <div class="weather">{{ weather.weather[0].main }}</div>
       </div>
     </div>
   </main>
@@ -19,12 +26,42 @@
 </template>
 
 <script>
-// 15:40
+
 export default {
   name: 'app',
   data () {
     return {
-      api_key: 'f68d0096290bce0149cd823809c08fd3'
+      api_key: 'f68d0096290bce0149cd823809c08fd3',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather(e){
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July",
+                    "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                  "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
@@ -46,6 +83,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url('./assets/warm-bg.jpg');
 }
 
 main {
